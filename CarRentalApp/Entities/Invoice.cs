fffe54b7cf2fcd4f2484938838fedfbc3698a49e
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-05-21 14:40:42 -0300

    Adicionar arquivos de projeto.

---
+++ CarRentalApp/Entities/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment   //propriedade calculada
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: " + BasicPayment.ToString("F2")
+                +"\nTax: " + Tax.ToString("F2")
+                +"\nTotal payment: " + TotalPayment.ToString("F2");
+        }
+    }
+}
---
+++ CarRentalApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using CarRentalApp.Entities;
+using CarRentalApp.Services;
+
+Console.WriteLine("Enter rental data: ");
+Console.WriteLine("Car model: ");
+string model = Console.ReadLine();
+Console.WriteLine("Pickup (dd/MM/yyyy hh:ss): ");
+DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+Console.WriteLine("Return (dd/MM/yyyy hh:ss): ");
+DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Enter price per hour: ");
+double hour = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter price per day: ");
+double day = double.Parse(Console.ReadLine());
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+RentalService rentalService = new RentalService(hour, day);
+
+rentalService.ProcessInvoice(carRental);
+
+Console.WriteLine("INVOICE: ");
+Console.WriteLine(carRental.Invoice);
